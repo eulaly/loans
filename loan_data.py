556ccf67dcd04d62e79e927471cc0d8e06c2fb34
eulaly <bmaugustus@gmail.com>
2020-11-01 12:08:04 -0500

    Add files via upload

---
+++ loan_data.py
@@ -0,0 +1,20 @@
+loans = [
+    {
+    "name" : "disc_6021",
+    "principal": 11369.59, 
+    "interest" : 7.375,
+    },
+    {
+    "name" : "disc_6023",
+    "principal": 6157.66, 
+    "interest" : 3.625,
+    },
+    {"name" : "lakes_797577",
+    "principal": 1436.05,
+    "interest": 5.350,
+    },
+    {"name": "lakes_798581",
+    "principal": 5285.06,
+    "interest": 5.00,
+    }
+]
